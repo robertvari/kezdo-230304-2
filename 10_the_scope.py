a7dedb0e40018437d39aef349f650fe0578b6d06
Robert Vari <mail.robertvari@gmail.com>
2023-03-18 15:28:58 +0100

    update

---
+++ 10_the_scope.py
@@ -0,0 +1,11 @@
+NAME = "Csaba"
+ADDRESS = "Pécs"
+
+def say_hello():
+    name = "Kriszta"
+    address = "Budapest"
+    print(f"Hello {name}")
+    print(f"Address local: {address}")
+    print(f"Address global: {ADDRESS}")
+
+say_hello()
