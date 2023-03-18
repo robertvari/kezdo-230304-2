321913fb27f9bbdf75db8d7f9d86cbb20dc64436
Robert Vari <mail.robertvari@gmail.com>
2023-03-18 14:32:27 +0100

    update

---
+++ 03_multiple_params.py
@@ -0,0 +1,8 @@
+def say_hello(name, email, address):
+    print("-"*50)
+    print(f"Hello {name}")
+    print(f"Email: {email}")
+    print(f"Address: {address}")
+    print("-"*50)
+
+say_hello("Csaba", "csaba@gmail.com", "Budapest")
