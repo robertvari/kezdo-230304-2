14134df522586e0bf35d3820d5e94d51bdf338d4
Robert Vari <mail.robertvari@gmail.com>
2023-03-19 17:22:53 +0100

    update

---
+++ PhotoData/PhotoData.py
@@ -0,0 +1,15 @@
+import os, json
+from PIL import Image
+
+def main():
+    # TODO get a valid folder path from the user
+    # TODO get all image files
+    # TODO collect data from images
+    # TODO save image data
+    pass
+
+def get_folder():
+    pass
+    # return a valid folder path
+
+main()
